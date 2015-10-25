1bae85c04d7b8634b6862f8522e5cbd7afa24e0a
Joran Le Cren <joran.lecren@gmail.com>
2015-10-25 17:33:49 +0100

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R contains a set of functions that can compute and cache the inverse
+## of matrices
 
-## Write a short comment describing this function
+## makeCacheMatrix returns an object that stores a matrix and
+## that can cache its inverse
+## x: a matrix
+## returns an object containing the cached inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of a matrix
+## and cache it if it was not already computed.
+## x: a matrix
+## ...: arguments for the function solve(x,...)
+## returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+## Test the function cacheSolve.
+
+testCacheSolve <- function() {
+  
+  mdat <- matrix(c(1,0,0, 0,2,0, 1,0,-1), nrow = 3, ncol = 3)
+  #minv <- matrix(c(1,0,1, 0,0.5,0, 0,0,-1), nrow = 3, ncol = 3)
+  mcache <- makeCacheMatrix(mdat)
+  stopifnot( is.null(mcache$getinverse()) )
+  cacheSolve(mcache)
+  stopifnot( !is.null(mcache$getinverse()) )
+  mcache$getinverse()
 }
